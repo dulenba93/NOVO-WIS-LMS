8f528f9e71b1f044c7e45622870c18ee0434cdac
AleksaV96 <32747026+AleksaV96@users.noreply.github.com>
2019-05-07 17:33:03 +0200

    dodati svi kontroleri i servisi sa osnovnim CRUD funkcijama

---
+++ LMS-Frontend/src/app/admin-service/admin.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from '../user';
+import { Observable } from 'rxjs';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+}; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  private BASE_URL = "http://localhost:8080"
+  private USER_URL = `${this.BASE_URL}\\users`;
+
+  constructor(private http: HttpClient) { }
+
+  addNewUser(user: User):Observable<any>{
+
+   return this.http.post(this.USER_URL, user);
+ 
+   
+  }
+}
