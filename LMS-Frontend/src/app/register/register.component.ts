718e5b03ba4fb12efdb711693d45d4bc6b258c19
MegaBurek <alekseykovacevic@gmail.com>
2019-05-11 18:44:25 +0200

    Added Form Validation for Registration Page
    Still having issues with displaying mat-error messages under inputs of form

--- LMS-Frontend/src/app/register/register.component.ts
+++ LMS-Frontend/src/app/register/register.component.ts
@@ -1,17 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../model/user'
-import { FormGroup, FormControl, FormBuilder, Validators, FormGroupDirective, NgForm } from '@angular/forms'
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 import { AdminService } from '../services/admin-service/admin.service';
-import { ErrorStateMatcher } from '@angular/material/core';
 
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const invalidCtrl = !!(control && control.invalid && control.parent.dirty);
-    const invalidParent = !!(control && control.parent && control.parent.invalid && control.parent.dirty);
-
-    return (invalidCtrl || invalidParent);
-  }
-}
 
 @Component({
   selector: 'app-register',
@@ -24,8 +15,6 @@ export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup;
 
-  matcher = new MyErrorStateMatcher();
-
   constructor(
     private adminService: AdminService,
     private fb: FormBuilder
@@ -34,27 +23,21 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
     this.registerForm = this.fb.group({
-      username: [null, Validators.required],
-      password: [null, Validators.required],
-      confirmPassword: [''],
+      username: [null, [Validators.required, Validators.minLength(6)]],
+      password: [null, [Validators.required, Validators.minLength(8)]],
+      confirmPassword: [null, Validators.minLength(8)],
       email: [null, [Validators.required, Validators.email]],
       role: [null, Validators.required]
-    }, { validator: this.checkPasswords });
+    });
   }
 
   onSubmit() {
-    const user = this.registerForm.value;
-    delete user['re_password'];
-    this.user = user;
-    this.adminService.addNewUser(this.user).subscribe();
-    alert("You have created a user")
-  }
+        const user = this.registerForm.value;
+        delete user['re_password'];
+        this.user = user;
+        this.adminService.addNewUser(this.user).subscribe();
+        alert("You have created a user")
+      }
 
-  checkPasswords(group: FormGroup) {
-    let pass = group.controls.password.value;
-    let confirmPass = group.controls.confirmPassword.value;
-
-    return pass === confirmPass ? null : { notSame: true }
-  }
 
 }
