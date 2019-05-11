1132841bdd8423ce5e21e9ea044e62495ae6b5e2
MegaBurek <alekseykovacevic@gmail.com>
2019-05-11 23:37:20 +0200

    Validation still not working for Register

--- LMS-Frontend/src/app/register/register.component.ts
+++ LMS-Frontend/src/app/register/register.component.ts
@@ -1,8 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../model/user'
-import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { FormGroup, FormBuilder, Validators, FormControl, FormGroupDirective, NgForm } from '@angular/forms'
 import { AdminService } from '../services/admin-service/admin.service';
+import { ErrorStateMatcher } from '@angular/material/core';
 
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const invalidCtrl = !!(control && control.invalid && control.parent.dirty);
+    const invalidParent = !!(control && control.parent && control.parent.invalid && control.parent.dirty);
+
+    return (invalidCtrl || invalidParent);
+  }
+}
 
 @Component({
   selector: 'app-register',
@@ -15,6 +24,8 @@ export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup;
 
+  matcher = new MyErrorStateMatcher();
+
   constructor(
     private adminService: AdminService,
     private fb: FormBuilder
@@ -25,19 +36,27 @@ export class RegisterComponent implements OnInit {
     this.registerForm = this.fb.group({
       username: [null, [Validators.required, Validators.minLength(6)]],
       password: [null, [Validators.required, Validators.minLength(8)]],
-      confirmPassword: [null, Validators.minLength(8)],
+      confirmPassword: [null, [Validators.required, Validators.minLength(8)]],
       email: [null, [Validators.required, Validators.email]],
       role: [null, Validators.required]
-    });
+    },
+      { validator: this.checkPasswords });
   }
 
   onSubmit() {
-        const user = this.registerForm.value;
-        delete user['re_password'];
-        this.user = user;
-        this.adminService.addNewUser(this.user).subscribe();
-        alert("You have created a user")
-      }
+    const user = this.registerForm.value;
+    delete user['re_password'];
+    this.user = user;
+    this.adminService.addNewUser(this.user).subscribe();
+    alert("You have created a user")
+  }
+
+  checkPasswords(group: FormGroup) { 
+    let pass = group.controls.password.value;
+    let confirmPass = group.controls.confirmPassword.value;
+
+    return pass === confirmPass ? null : { notSame: true }
+  }
 
 
 }
